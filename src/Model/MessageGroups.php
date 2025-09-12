9b08447ed6f1b6582018fb167a34f4fe983c006d
Andre Flitsch <pixelhed@pixelperfect.at>
2025-09-12 12:36:42 +0200

    Add Classes for MessageGroup and SessionKey constants
    Signed-off-by: Andre Flitsch <pixelhed@pixelperfect.at>

---
+++ src/Model/MessageGroups.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace PixelPerfect\DiscountExclusion\Model;
+
+class MessageGroups
+{
+    public const string DISCOUNT_EXCLUSION = 'discount_exclusion';
+}
---
+++ src/Model/SessionKeys.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace PixelPerfect\DiscountExclusion\Model;
+
+class SessionKeys
+{
+    public const string PROCESSED_PRODUCT_IDS = 'processed_product_ids';
+}
